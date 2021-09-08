72b3933c096a20c9f8ac46c81640fb9d1c43d334
Jan Schmitt <jan@smittie.de>
2021-09-08 08:11:58 +0200

    feat: adjust model to support different lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -28,8 +28,9 @@ class ListViewTest(TestCase):
 
     def test_displays_all_list_items(self):
         # GIVEN
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        new_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=new_list)
+        Item.objects.create(text='itemey 2', list=new_list)
         # WHEN
         response = self.client.get('/lists/the-only-list-in-the-world/')
         # THEN
@@ -37,15 +38,23 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        new_list = List()
+        new_list.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = new_list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = new_list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, new_list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -53,4 +62,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, new_list)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, new_list)
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from lists.models import Item
+from lists.models import Item, List
 from django.shortcuts import render, redirect
 
 
@@ -12,5 +12,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    new_list = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=new_list)
     return redirect('/lists/the-only-list-in-the-world/')
