1133aa8cfa68d34d9f4e0e2f2232742aafe7c065
Jan Schmitt <jan@smittie.de>
2021-09-19 13:28:20 +0200

    chore(setup): differentiate between dev and live by using env vars.

--- superlists/settings.py
+++ superlists/settings.py
@@ -18,13 +18,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x%zv0p+52-kif1@khpn*a5ki!!2ul=wai2^d!186$-+vvi1&(6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'x%zv0p+52-kif1@khpn*a5ki!!2ul=wai2^d!186$-+vvi1&(6'
+    ALLOWED_HOSTS = []
 
 # Application definition
 
