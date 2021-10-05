15f2ca01488b05ff723e1fe89a1e3044215ac2cd
Jan Schmitt <jan@smittie.de>
2021-10-05 15:12:39 +0200

    test: generic help waiter and new ft for empty to-do items

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,15 +6,31 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the homepage and accidentally tries to submit
         # an empty list item. She hits Enter on the empty inputbox.
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # The homepage refreshes, and there is an error message saying
         # that list items cannot be blank.
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"))
 
         # She tries again with some text for the item, which now works
+        self.browser.find_element_by_id('id_new_item').send_keys(
+            'Buy coffee beans')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy coffee beans')
 
         # Perversely, she now decides to submit a second blank list item
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"))
 
         # And she can corret it by filling some text in
-        self.fail('write me!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Brew coffee')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy coffee beans')
+        self.wait_for_row_in_list_table('2: Brew coffee')
