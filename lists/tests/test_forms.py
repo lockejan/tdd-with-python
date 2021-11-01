a4be0ee3b9ea7cbb8f4b4284d39549a7383a6cd8
Jan Schmitt <jan@smittie.de>
2021-11-01 21:20:54 +0100

    wip: use new django form in home_page and simplify tests.
    currently the new form breaks tests due to missing id's

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -12,3 +12,7 @@ class ItemFormTest(TestCase):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
