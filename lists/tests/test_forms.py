a3ec363fc3055e18e124f117c54bd844804892c5
Jan Schmitt <git@smittie.de>
2022-01-25 21:18:06 +0100

    refactor(views): remove duplication of validation logic

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -12,7 +12,3 @@ class ItemFormTest(TestCase):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
-
-    def test_home_page_uses_item_form(self):
-        response = self.client.get('/')
-        self.assertIsInstance(response.context['form'], ItemForm)
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ValidationError
 from lists.forms import ItemForm
 from lists.models import Item, List
 from django.shortcuts import render, redirect
@@ -8,29 +7,22 @@ def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
 
+def new_list(request):
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        new_list = List.objects.create()
+        Item.objects.create(text=request.POST['text'], list=new_list)
+        return redirect(new_list)
+    else:
+        return render(request, 'home.html', {"form": form})
+
+
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
-
+    form = ItemForm()
     if request.method == 'POST':
-        try:
-            item = Item(text=request.POST['text'], list=list_)
-            item.full_clean()
-            item.save()
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST['text'], list=list_)
             return redirect(list_)
-        except ValidationError:
-            error = "You can't have an empty list item"
-
-    return render(request, 'list.html', {'list': list_, 'error': error})
-
-
-def new_list(request):
-    new_list = List.objects.create()
-    item = Item.objects.create(text=request.POST['text'], list=new_list)
-    try:
-        item.full_clean()
-    except ValidationError:
-        new_list.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error})
-    return redirect(new_list)
+    return render(request, 'list.html', {'list': list_, 'form': form})
