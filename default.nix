dc9bf662194d5691c0e6c5b7c1b2a60c83d72243
Jan Schmitt <git@smittie.de>
2022-04-02 11:25:28 +0200

    chore: nix setup with shellHooks :technologist: 🚀
    * the whole dev env gets provisioned on dir visit🚀

---
+++ default.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> { };
+mkShell {
+  nativeBuildInputs = [ bashInteractive python37 python37Packages.pip ];
+
+  shellHook = ''
+    echo "Using ${python37.name}"
+
+    [ ! -d '$PROJDIR/python-dev' ] && python -m venv python-dev && echo "SETUP python-dev: DONE"
+    source python-dev/bin/activate
+    python -m pip install -r requirements-dev.txt
+    set -a; source .env; set +a
+  '';
+}
