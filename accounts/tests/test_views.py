ede86475f3162f221ce9a8eb4fcf41fddfdc75dc
Jan Schmitt <git@smittie.de>
2022-04-07 22:51:41 +0200

    fix(test): wrong mail in view test

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -20,7 +20,7 @@ class SendLoginEmailView(TestCase):
         self.assertEqual(mock_send_mail.called, True)
         (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
         self.assertEqual(subject, 'Your login link for Superlists')
-        self.assertEqual(from_email, 'superlists@smittie.de')
+        self.assertEqual(from_email, 'noreply@smittie.de')
         self.assertEqual(to_list, ['edith@example.com'])
 
     def test_adds_success_message(self):
