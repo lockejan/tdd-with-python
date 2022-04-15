576e631b9c56e4bd4db1262ef3d1629e7a7b6171
Jan Schmitt <git@smittie.de>
2022-04-15 14:00:03 +0200

    refacort: use nix flakes for providing a dev-shell :technologist: 🚀

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A basic flake with a dev shell for a python37 django toy project";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            bashInteractive
+            python37
+            python37Packages.pip
+            ansible
+          ];
+          buildInputs = [ ];
+          shellHook = ''
+            [ ! -d '$PROJDIR/python-dev' ] && python -m venv python-dev && echo "SETUP python-dev: DONE"
+            source python-dev/bin/activate
+            python -m pip install --upgrade pip
+            python -m pip install -r requirements-dev.txt
+            set -a; source .env; set +a
+          '';
+        };
+      });
+}
