0e48624865b652f43ae3489f3307c8ec744d8c5e
Jan Schmitt <git@smittie.de>
2022-06-18 21:45:10 +0200

    test: refactor entering items to lists
    * introduce helper fn add_list_item
    * write function test for personalized lists

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -17,11 +17,7 @@ class ItemValidationTest(FunctionalTest):
             '#id_text:invalid'))
 
         # She tries again with some text for the item, which now works
-        self.get_item_input_box().send_keys('Buy coffee beans')
-        self.wait_for(lambda: self.browser.find_element_by_css_selector(
-            '#id_text:valid'))
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy coffee beans')
+        self.add_list_item("Buy coffee beans")
 
         # Perversely, she now decides to submit a second blank list item
         self.get_item_input_box().send_keys(Keys.ENTER)
@@ -31,7 +27,7 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for(lambda: self.browser.find_element_by_css_selector(
             '#id_text:invalid'))
 
-        # And she can corret it by filling some text in
+        # And she can correct it by filling some text in
         self.get_item_input_box().send_keys('Brew coffee')
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy coffee beans')
@@ -40,9 +36,7 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_duplicate_items(self):
         # Edith goes to the home page and starts a new list
         self.browser.get(self.live_server_url)
-        self.get_item_input_box().send_keys('Buy coffee beans')
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy coffee beans')
+        self.add_list_item("Buy coffee beans")
 
         # She tries to enter the same item again
         self.get_item_input_box().send_keys('Buy coffee beans')
@@ -56,9 +50,7 @@ class ItemValidationTest(FunctionalTest):
     def test_error_messages_are_cleared_on_input(self):
         # Edith starts a list and causes a validation error:
         self.browser.get(self.live_server_url)
-        self.get_item_input_box().send_keys('Buy coffee beans')
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy coffee beans')
+        self.add_list_item("Buy coffee beans")
         self.get_item_input_box().send_keys('Buy coffee beans')
         self.get_item_input_box().send_keys(Keys.ENTER)
 
